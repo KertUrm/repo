206c0cd805bdb39e0eca551947c692ea74eac5fe
KertUrm <118337171+KertUrm@users.noreply.github.com>
2022-11-16 10:08:55 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,15 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            var a = 0;
-            while (a < 10000)
+            //var a = 0;
+            //while (a < 10000)
+            //{
+            //    Console.WriteLine("abc");
+            //    a++;
+            //}
+            for (int i = 0; i < 10000; i++)
             {
                 Console.WriteLine("abc");
-                a++;
             }
             
         }
